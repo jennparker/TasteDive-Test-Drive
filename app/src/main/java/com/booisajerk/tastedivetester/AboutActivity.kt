93c956346e0d8340c39f8a2f5e926c1fbe77aec4
Jenn Parker <jenniferparker070@gmail.com>
2018-12-06 11:18:27 -0800

    Update button style, add button to AboutActivity

--- app/src/main/java/com/booisajerk/tastedivetester/AboutActivity.kt
+++ app/src/main/java/com/booisajerk/tastedivetester/AboutActivity.kt
@@ -1,6 +1,9 @@
 package com.booisajerk.tastedivetester
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class AboutActivity : AppCompatActivity() {
@@ -8,5 +11,16 @@ class AboutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        showTastediveButton()
+    }
+
+    private fun showTastediveButton() {
+        val tasteDiveButton: Button = findViewById(R.id.openTasteDiveAbout)
+        tasteDiveButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(Constants.BASE_URL)
+            startActivity(intent)
+        }
     }
 }
